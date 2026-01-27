fd334d69ceb373dfe7739b8fc250dbbcc48497fc
mohamed amine <mohamedaminewwe@gmail.com>
2026-01-27 23:32:16 +0100

    created the html and the majority of js , still need to add the logic for the remove and read status button and also the styling

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Library
+
+a project where i practice using objects  and object constructors by creating a library where you can add a book having a title , author's name , number of pages and if you have read it or no , you can then delete it if you want
---
+++ script.js
@@ -0,0 +1,87 @@
+let mylibrary = []
+
+function Book(title,author,num_pages,read) {
+    this.id = crypto.randomUUID(),
+    this.title = title,
+    this.author = author,
+    this.num_pages = num_pages,
+    this.read = read
+}
+
+function addBookToLibrary(title,author,num_pages,read,mylibrary){
+    const book = new Book(title,author,num_pages,read)
+    mylibrary.push(book)
+}
+
+function display_books(mylibrary){
+    const libraryCont = document.getElementById("library")
+    libraryCont.innerHTML = ''
+
+    for (let i = 0 ; i < mylibrary.length ; i++){
+        const newBook = document.createElement("div")
+        newBook.classList.add("book-card")
+
+        const heading = document.createElement("H3")
+        const author = document.createElement("p")
+        const numpages = document.createElement("p")
+        const readstat = document.createElement("p")
+        const changestat = document.createElement("button")
+        const remove = document.createElement("button")
+
+        newBook.dataset.id = mylibrary[i].id
+
+        heading.textContent = `Title : ${mylibrary[i].title}`
+        author.textContent = `author : ${mylibrary[i].author}`
+        numpages.textContent = `number of pages : ${mylibrary[i].num_pages}`
+        readstat.textContent = `status : ${mylibrary[i].read}`
+        changestat.textContent = "Read/Not Read"
+        remove.textContent = "Remove book"
+
+        newBook.appendChild(heading)
+        newBook.appendChild(author)
+        newBook.appendChild(numpages)
+        newBook.appendChild(readstat)
+        newBook.appendChild(changestat)
+        newBook.appendChild(remove)
+
+        libraryCont.appendChild(newBook)
+    }
+}
+
+const New_btn = document.createElement("button")
+
+New_btn.dataset.id = "newbtn"
+New_btn.textContent = "New Book"
+
+const dialog = document.querySelector("dialog")
+const form = document.querySelector("form")
+
+New_btn.addEventListener("click", ()=>{
+    dialog.showModal()
+})
+
+document.querySelector(".close").addEventListener("click",()=>{
+    dialog.close()
+})
+
+form.addEventListener("submit",(e) => {
+    e.preventDefault();
+
+    const title = document.getElementById("title").value
+    const Author = document.getElementById("author").value
+    const npages = Number(document.getElementById("number_pages").value)
+    const readChoice = document.getElementById("read").checked
+
+    if (!title || !Author || npages <= 0) return
+
+    addBookToLibrary(title,Author,npages,readChoice,mylibrary)
+    display_books(mylibrary)
+
+    form.reset()
+    dialog.close()
+})
+
+
+document.querySelector("header").appendChild(New_btn)
+
+
